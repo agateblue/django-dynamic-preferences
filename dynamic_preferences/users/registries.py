95b0f5639bc520ea6bcc0db89045e462cb72f9a8
jordiromera <jordiromera@users.sourceforge.net>
2018-09-19 10:58:51 +0200

    Fix UserPreferenceRegistry and its 'section_url_namespace' attribute

--- dynamic_preferences/users/registries.py
+++ dynamic_preferences/users/registries.py
@@ -2,7 +2,7 @@ from ..registries import PerInstancePreferenceRegistry
 
 
 class UserPreferenceRegistry(PerInstancePreferenceRegistry):
-    section_url_namespace = 'dynamic_preferences.user.section'
+    section_url_namespace = 'dynamic_preferences:user.section'
 
 
 user_preferences_registry = UserPreferenceRegistry()
