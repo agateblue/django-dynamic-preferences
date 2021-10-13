fb2e7a3c8aeba07a6a8b4c24ac3a805cabaee56a
Cleiton Lima <cleiton.limapin@gmail.com>
2021-10-13 11:12:12 -0300

    Change logging for stdout in checkpreferences command

--- dynamic_preferences/management/commands/checkpreferences.py
+++ dynamic_preferences/management/commands/checkpreferences.py
@@ -1,22 +1,23 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from dynamic_preferences.exceptions import NotFoundInRegistry
 from dynamic_preferences.models import GlobalPreferenceModel
-from dynamic_preferences.registries import preference_models, global_preferences_registry
-
-import logging
-logger = logging.getLogger(__name__)
+from dynamic_preferences.registries import (global_preferences_registry,
+                                            preference_models)
 
 
 def delete_preferences(queryset):
     """
-    Delete preferences objects if they are not present in registry. Return a list of deleted objects
+    Delete preferences objects if they are not present in registry.
+    Return a list of deleted objects
     """
     deleted = []
 
-    # Iterate through preferences. If an error is raised when accessing preference object, just delete it
+    # Iterate through preferences. If an error is raised when accessing
+    # preference object, just delete it
     for p in queryset:
         try:
-            pref = p.registry.get(section=p.section, name=p.name, fallback=False)
+            p.registry.get(
+                section=p.section, name=p.name, fallback=False)
         except NotFoundInRegistry:
             p.delete()
             deleted.append(p)
@@ -25,26 +26,42 @@ def delete_preferences(queryset):
 
 
 class Command(BaseCommand):
-    help = "Find and delete preferences from database if they don't exist in registries. Create preferences that are " \
-           "not present in database"
+    help = (
+        "Find and delete preferences from database if they don't exist in "
+        "registries. Create preferences that are not present in database"
+    )
 
     def handle(self, *args, **options):
 
         # Create needed preferences
         # Global
-        logger.info('Creating missing global preferences...')
+        self.stdout.write("Creating missing global preferences...")
         manager = global_preferences_registry.manager()
         manager.all()
 
         deleted = delete_preferences(GlobalPreferenceModel.objects.all())
-        logger.info("Deleted {0} global preferences".format(len(deleted)))
+        message = "Deleted {deleted} global preferences".format(
+            deleted=len(deleted)
+        )
+        self.stdout.write(message)
 
         for preference_model, registry in preference_models.items():
             deleted = delete_preferences(preference_model.objects.all())
-            logger.info("Deleted {0} {1} preferences".format(len(deleted), preference_model.__name__))
+            message = (
+                "Deleted {deleted} {model} preferences".format(
+                    deleted=len(deleted),
+                    model=preference_model.__name__,
+                )
+            )
+            self.stdout.write(message)
             if not hasattr(preference_model, 'get_instance_model'):
                 continue
 
-            logger.info('Creating missing preferences for {0} model...'.format(preference_model.get_instance_model().__name__))
+            message = (
+                "Creating missing preferences for {model} model...".format(
+                    model=preference_model.get_instance_model().__name__,
+                )
+            )
+            self.stdout.write(message)
             for instance in preference_model.get_instance_model().objects.all():
                 instance.preferences.all()
