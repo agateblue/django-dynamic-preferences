83007b16ba63eb15ca85ceaf12b9ecc4403a3b13
Cleiton Lima <cleiton.limapin@gmail.com>
2022-03-02 16:17:21 -0300

    Fix default_app_config deprecation

--- dynamic_preferences/__init__.py
+++ dynamic_preferences/__init__.py
@@ -1,2 +1,6 @@
+import django
+
 __version__ = "1.12.0"
-default_app_config = "dynamic_preferences.apps.DynamicPreferencesConfig"
+
+if django.VERSION < (3, 2):
+    default_app_config = "dynamic_preferences.apps.DynamicPreferencesConfig"
