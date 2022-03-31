2427ffb55262872d32d82d96681e723ba200c84f
Agate <me@agate.blue>
2022-03-31 20:18:36 +0200

    Revert "Fix default_app_config deprecation"
    This reverts commit 83007b16ba63eb15ca85ceaf12b9ecc4403a3b13.

--- dynamic_preferences/__init__.py
+++ dynamic_preferences/__init__.py
@@ -1,6 +1,2 @@
-import django
-
 __version__ = "1.12.0"
-
-if django.VERSION < (3, 2):
-    default_app_config = "dynamic_preferences.apps.DynamicPreferencesConfig"
+default_app_config = "dynamic_preferences.apps.DynamicPreferencesConfig"
